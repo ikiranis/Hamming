4b3a4708668a6155caa091be7efc533df47348da
rocean <rocean74@gmail.com>
2022-10-24 02:19:36 +0300

    Generate arrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Random;
+
 public class Main {
+    private static final int N = 16777216;   // Μέγεθος array (2^24)
+    private static final Random random = new Random();
+
+    /**
+     * Δημιουργία array με τυχαίες τιμές 0,1
+     *
+     * @return int[]
+     */
+    private static int[] generateArray() {
+        int[] array = new int[N];
+
+        for(int i=0; i<N; i++) {
+            array[i] = random.nextInt(2);
+        }
+
+        return array;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] A = generateArray();
+        int[] B = generateArray();
+
     }
 }
